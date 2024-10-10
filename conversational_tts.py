febf38dda18babc14c0fe9ac2b6442631a5d8b52
Amartya Varma <amartya.varma@gmail.com>
2024-10-10 12:14:23 -0700

    f

--- conversational_tts.py
+++ conversational_tts.py
@@ -255,4 +255,4 @@ columns_to_remove = [col for col in all_columns if col not in columns_to_keep]
 dataset_to_upload = full_processed_padded.remove_columns(columns_to_remove)
 
 # Now upload the dataset with only the desired columns
-dataset_to_upload.push_to_hub("amuvarma/conversation-tune-13k")
+dataset_to_upload.push_to_hub("amuvarma/conversation-tune-13k-25kl")
