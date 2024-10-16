d9119304d61e61d982a27951ede828caed508c1c
Amartya Varma <amartya.varma@gmail.com>
2024-10-16 00:45:54 -0700

    f

--- tags-tts-llamaemo.py
+++ tags-tts-llamaemo.py
@@ -144,4 +144,4 @@ columns_to_remove = [col for col in all_columns if col not in columns_to_keep]
 dataset_to_upload = full_processed_padded.remove_columns(columns_to_remove)
 
 # Now upload the dataset with only the desired columns
-dataset_to_upload.push_to_hub("amuvarma/5200-emo-instruct-3dups")
+dataset_to_upload.push_to_hub("amuvarma/luna-trejo-novad-0-wdups")
