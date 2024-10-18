d295c6d084bb30a8beff75a3d83eb0cef4cd357b
Amartya Varma <amartya.varma@gmail.com>
2024-10-18 00:55:43 -0700

    F

--- tags-tts-llamaemo.py
+++ tags-tts-llamaemo.py
@@ -5,7 +5,7 @@ from datasets import concatenate_datasets
 import random
 
 tkn = "meta-llama/Llama-3.2-3B-Instruct"
-dsn = "eliasfiz/audio-lunatrejo-0_facodec"
+dsn = "eliasfiz/audio-briancarrillo-16k-0_facodec"
 
 
 
@@ -144,4 +144,4 @@ columns_to_remove = [col for col in all_columns if col not in columns_to_keep]
 dataset_to_upload = full_processed_padded.remove_columns(columns_to_remove)
 
 # Now upload the dataset with only the desired columns
-dataset_to_upload.push_to_hub("amuvarma/luna-trejo-vad-no-emo-1")
+dataset_to_upload.push_to_hub("amuvarma/brian-carrillo-16k-noemo-0")
