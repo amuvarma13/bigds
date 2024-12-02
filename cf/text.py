7cb32ac4146f7db76291db8bef20f6860a84a171
Amartya Varma <amartya.varma@gmail.com>
2024-12-01 20:32:39 -0800

    f

--- cf/text.py
+++ cf/text.py
@@ -14,7 +14,7 @@ push_name = "amuvarma/orcatext-dev-processed-1"
 ds_name = "amuvarma/combined-shuffled-text-orca-ds"
 ds = load_dataset(ds_name, split="train")
 
-ds = ds.select(range(1000))
+
  
 
 tokeniser_length = 128256
