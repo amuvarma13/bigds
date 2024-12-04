3e564f633c601410e3da3d4dbc73f7cad5a227d2
Amartya Varma <amartya.varma@gmail.com>
2024-12-04 12:43:35 -0800

    f

--- older/3_dup_dataset.py
+++ older/3_dup_dataset.py
@@ -68,8 +68,8 @@ def remove_excess_consecutive_integers(dataset, column_name, facodec_columns):
 # Use the function
 dataset = ds  # Assuming you want to process the 'train' split
 
-processed_dataset = remove_excess_consecutive_integers(dataset, 'facodec_1', ['ass1_facodec_1','ass1_facodec_0', 'ass1_facodec_2', 'ass1_facodec_3', 'ass1_facodec_4', 'ass1_facodec_5'])
-processed_dataset = remove_excess_consecutive_integers(processed_dataset, 'facodec_1', ['ass2_facodec_1','ass2_facodec_0', 'ass2_facodec_2', 'ass2_facodec_3', 'ass2_facodec_4', 'ass2_facodec_5'])
-processed_dataset = remove_excess_consecutive_integers(processed_dataset, 'facodec_1', ['ass3_facodec_1','ass3_facodec_0', 'ass3_facodec_2', 'ass3_facodec_3', 'ass3_facodec_4', 'ass3_facodec_5'])
+processed_dataset = remove_excess_consecutive_integers(dataset, 'ass1_facodec_1', ['ass1_facodec_1','ass1_facodec_0', 'ass1_facodec_2', 'ass1_facodec_3', 'ass1_facodec_4', 'ass1_facodec_5'])
+processed_dataset = remove_excess_consecutive_integers(processed_dataset, 'ass1_facodec_2', ['ass2_facodec_1','ass2_facodec_0', 'ass2_facodec_2', 'ass2_facodec_3', 'ass2_facodec_4', 'ass2_facodec_5'])
+processed_dataset = remove_excess_consecutive_integers(processed_dataset, 'ass3_facodec_1', ['ass3_facodec_1','ass3_facodec_0', 'ass3_facodec_2', 'ass3_facodec_3', 'ass3_facodec_4', 'ass3_facodec_5'])
 
 processed_dataset.push_to_hub(push_name)
