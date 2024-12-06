6844f952d7e0ce34ec29a295a82d32851d4f9ba0
Amartya Varma <amartya.varma@gmail.com>
2024-12-06 10:48:14 -0800

    f

--- voice_ass.py
+++ voice_ass.py
@@ -1,3 +1,7 @@
 from datasets import load_dataset
 dsn = "gpt-omni/VoiceAssistant-400K"
 ds = load_dataset(dsn)
+
+dsnd = ds["train"].select(range(10000))
+
+dsnd.push_to_hub("amuvarma/voice-assistant-10k-processed-1")
