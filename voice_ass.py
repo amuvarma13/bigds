8902692e88218e178c2b2ce9afb13a6c072db78c
Amartya Varma <amartya.varma@gmail.com>
2024-12-07 20:28:26 -0800

    f

--- voice_ass.py
+++ voice_ass.py
@@ -3,7 +3,8 @@ dsn = "gpt-omni/VoiceAssistant-400K"
 ds = load_dataset(dsn)
 ds = ds["train"].remove_columns(['split_name', 'index', 'round', 'answer_snac'])
 ds = ds.shuffle(seed=42)
-dsnd = ds.select(range(200000, 250000))
+# dsnd = ds.select(range(200000, 250000))
+dsnd = ds.select(range(250000, 300000))
 
 # print(dsnd)
-dsnd.push_to_hub("amuvarma/voice-assistant-200-250k-processed")
+dsnd.push_to_hub("amuvarma/voice-assistant-250-300k-processed")
