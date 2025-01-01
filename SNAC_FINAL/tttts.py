8ea8fbf528cdf23161fd6724391fa83e9a8668db
Amartya Varma <amartya.varma@gmail.com>
2025-01-01 20:26:12 +0000

    f

--- SNAC_FINAL/tttts.py
+++ SNAC_FINAL/tttts.py
@@ -34,20 +34,10 @@ tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 # columns_to_remove = [col for col in ds.column_names if col != "answer_snac"]
 # ds = ds.remove_columns(columns_to_remove)
 
-def convert_string_to_codes(example):
-    snac_str = example["answer_snac"]
-    parts = snac_str.split('#')
-    parts = [p.strip() for p in parts if p.strip()]
-    snac_lols = [list(map(int, p.split())) for p in parts]
-    example["snac_lols"] = snac_lols
-    return example
-
 # Determine number of processes based on CPU count
 num_proc = os.cpu_count() - 2
 
 # Map the function in parallel
-ds = ds.map(convert_string_to_codes, num_proc=num_proc)
-
 def tokenize_fn(example):
     user_ids = tokenizer.encode(example["question"], add_special_tokens=True)
     answer_ids = tokenizer.encode(example["answer"], add_special_tokens=True)
