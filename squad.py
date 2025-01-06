e08bd6a8074145e4f26b02654aed9a7190ddef9e
Amartya Varma <amartya.varma@gmail.com>
2025-01-06 21:24:39 +0000

    f

--- squad.py
+++ squad.py
@@ -5,8 +5,8 @@ from datasets import concatenate_datasets
 import random
 
 tkn = "meta-llama/Llama-3.2-3B-Instruct"
-dsn = "amuvarma/va-320k-330k-snac-no-identity"
-pushname = "amuvarma/va-320k-330k-snac-no-identity-QA_TTTTS"
+dsn = "amuvarma/zuck-nopunc-text"
+pushname = "amuvarma/zuck-nopunc-text-TTT"
 
 tokenizer = AutoTokenizer.from_pretrained(tkn)
 
