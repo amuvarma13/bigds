e2a60f03583112381b1fe1075e0d7207d723f7b3
Amartya Varma <amartya.varma@gmail.com>
2025-01-14 15:09:34 -0800

    f

--- SNAC_FINAL/combine_rows.py
+++ SNAC_FINAL/combine_rows.py
@@ -4,7 +4,7 @@ import os
 num_proc = os.cpu_count() -2
 
 dsn = "amuvarma/text-messages-6m-processed-1"
-push_name = "amuvarma/text-messages-6m-processed-combined-trunc"
+push_name = "amuvarma/text-messages-6m-processed-combinedfor10m-trunc"
 
 dataset = load_dataset(dsn, split='train')
 
