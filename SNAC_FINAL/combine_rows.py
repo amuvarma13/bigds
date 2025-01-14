db7aa87e4b3dee79bf75944c01a6b50a4504a14c
Amartya Varma <amartya.varma@gmail.com>
2025-01-14 13:35:31 -0800

    f

--- SNAC_FINAL/combine_rows.py
+++ SNAC_FINAL/combine_rows.py
@@ -1,13 +1,12 @@
 from datasets import Dataset, load_dataset
 
-# For illustration, let's suppose `dataset` is already loaded:
-# dataset = load_dataset(...)["train"]
-# and it has columns: 'input_ids', 'attention_mask', 'labels', each of which is a list of ints.
-
-dsn = "amuvarma/zuck-nopunc-wcodes-TTTTS"
-push_name = "amuvarma/zuck-nopunc-wcodes-TTTTS-merged"
+dsn = "amuvarma/text-messages-6m-processed-1"
+push_name = "amuvarma/text-messages-6m-processed-combined"
 
 dataset = load_dataset(dsn, split='train')
+
+# Remove columns not 'input_ids', 'attention_mask', 'labels'
+dataset = dataset.remove_columns([col for col in dataset.column_names if col not in ['input_ids', 'attention_mask', 'labels']])
 
 def merge_4_samples(examples):
 
@@ -15,8 +14,6 @@ def merge_4_samples(examples):
     merged_attention_mask = []
     merged_labels = []
     
-    # We can simply use Python's sum over lists to flatten:
-    # sum(list_of_lists, []) concatenates them.
     merged_input_ids = sum(examples["input_ids"], [])
     merged_attention_mask = sum(examples["attention_mask"], [])
     merged_labels = sum(examples["labels"], [])
@@ -33,8 +30,7 @@ merged_dataset = dataset.map(
     merge_4_samples,
     batched=True,
     batch_size=4,
-    # optionally remove the old columns if you only want the merged ones:
-    # remove_columns=dataset.column_names
+
 )
 
 print(merged_dataset)
