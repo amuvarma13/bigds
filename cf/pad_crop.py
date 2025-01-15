0646c641c5e5130dac741470a5897c6e6554d5d9
Amartya Varma <amartya.varma@gmail.com>
2025-01-14 16:48:11 -0800

    f

--- cf/pad_crop.py
+++ cf/pad_crop.py
@@ -9,7 +9,6 @@ push_name = "amuvarma/snac-2m-tts-2g-3328l"
 ds = load_dataset(dsn, split='train')
 
 max_length = 3328
-pad_token = 128263
 
 num_cpus = os.cpu_count()
 
@@ -21,13 +20,13 @@ def pad_and_create_mask(example):
         example['attention_mask'] = [1] * max_length
         example["labels"] = example["input_ids"][:max_length]
     else:
-        padding_length = max_length - len(example['input_ids'])
-        example['attention_mask'] = [1] * len(example['input_ids']) + [0] * padding_length
-        example["labels"] = example["input_ids"] + [-100] * padding_length
-        example['input_ids'] = example['input_ids'] + [pad_token] * padding_length
-        
+        example['attention_mask'] = [1] * len(example['input_ids'])
+        example["labels"] = example["input_ids"]
+        example['input_ids'] = example['input_ids']
 
     return example
+
+
 ds_1 = ds.map(pad_and_create_mask, num_proc=num_processes)
 
 ds_1.push_to_hub(push_name)
