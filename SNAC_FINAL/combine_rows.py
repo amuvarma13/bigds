8df6ae50752f375227139b3a342447a038fc83cb
Amartya Varma <amartya.varma@gmail.com>
2025-01-14 16:32:24 -0800

    f

--- SNAC_FINAL/combine_rows.py
+++ SNAC_FINAL/combine_rows.py
@@ -4,28 +4,24 @@ import os
 num_proc = os.cpu_count() -2
 
 dsn = "amuvarma/snac-2m-tts"
-push_name = "amuvarma/snac-2m-tts-3g"
+push_name = "amuvarma/snac-2m-tts-2g"
 
 dataset = load_dataset(dsn, split='train')
 
 # Remove columns not 'input_ids', 'attention_mask', 'labels'
-dataset = dataset.remove_columns([col for col in dataset.column_names if col not in ['input_ids', 'attention_mask', 'labels']])
+dataset = dataset.remove_columns([col for col in dataset.column_names if col not in ['input_ids']])
 
 def merge_4_samples(examples):
 
     merged_input_ids = []
-    merged_attention_mask = []
-    merged_labels = []
+
     
     merged_input_ids = sum(examples["input_ids"], [])
-    merged_attention_mask = sum(examples["attention_mask"], [])
-    merged_labels = sum(examples["labels"], [])
+
 
     # Return them each as a *list of length 1* so map creates 1 new row, not 4.
     return {
         "input_ids": [merged_input_ids],
-        "attention_mask": [merged_attention_mask],
-        "labels": [merged_labels],
     }
 
 # Map the dataset using a batch size of 4
