43276da5960410957352d0ed537c6fad837ec207
Amartya Varma <amartya.varma@gmail.com>
2025-01-31 15:07:31 -0800

    f

--- process_omni_dataset.py
+++ process_omni_dataset.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
-def _load_dataset(self, dataset_name):
+def _load_dataset(dataset_name):
     snapshot_download(
         repo_id=dataset_name,
         repo_type="dataset",   
