76a873f095f842a7feef1c31a1a9b4b9d5c527d6
Amartya Varma <amartya.varma@gmail.com>
2025-02-26 14:05:36 -0800

    f

--- calculate_ratios.py
+++ calculate_ratios.py
@@ -2,8 +2,8 @@ from datasets import load_dataset
 import os
 
 # Load your dataset
-dsn = "amuvarma/voice-assistant-adapted-3-100k-snacced"
-push_name = "amuvarma/voice-assistant-adapted-3-100k-snacced-ratio"
+dsn = "amuvarma/regconvos-kokoro"
+push_name = "amuvarma/regconvos-kokoro-ratio"
 
 ds = load_dataset(dsn, split="train")
 
