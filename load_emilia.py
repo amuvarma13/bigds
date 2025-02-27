bd4771f9983e06aa84f88535b3d34a16b619ac77
Amartya Varma <amartya.varma@gmail.com>
2025-02-27 11:30:42 -0800

    f

--- load_emilia.py
+++ load_emilia.py
@@ -6,6 +6,11 @@ from datasets import load_dataset
 repo_id = "amphion/Emilia-Dataset"
 path = "Emilia/EN/*.tar"
 dataset = load_dataset("amphion/Emilia-Dataset", data_files={"en": path}, split="en")
+
+dataset = dataset.select(range(10))
+
+
+
 print(dataset[0]["json"])
 from datasets import Dataset, Audio
 from tqdm import tqdm
