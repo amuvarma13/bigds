b250b9f4f53496b18e134f858bf1f8351d34c265
Amartya Varma <amartya.varma@gmail.com>
2025-02-26 16:52:10 -0800

    f

---
+++ load_emilia.py
@@ -0,0 +1,15 @@
+from datasets import load_dataset
+
+from huggingface_hub import snapshot_download
+from datasets import load_dataset
+
+repo_id = "amphion/Emilia-Dataset"
+
+snapshot_download(
+    repo_id=repo_id,
+    repo_type="dataset",   
+    revision="main",        
+    max_workers=64         
+)
+
+load_dataset(repo_id, split="train")
