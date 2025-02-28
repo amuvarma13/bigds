4cab921b28c7c0c3361f9f752c480e8e2b491ad4
Amartya Varma <amartya.varma@gmail.com>
2025-02-28 10:28:14 -0800

    f

--- SNAC_FINAL/combine_rows_8192.py
+++ SNAC_FINAL/combine_rows_8192.py
@@ -1,4 +1,4 @@
-from datasets import Dataset, load_dataset
+from datasets import Dataset, load_dataset, concatenate_datasets
 from itertools import chain
 import os
 import time
@@ -7,13 +7,18 @@ import time
 dsn = "amuvarma/emilia-30k-TTS"
 dataset = load_dataset(dsn, split='train')
 
+#get length of dataset
+dataset_length = len(dataset)
+print(f"Length of dataset: {dataset_length}")
+
 dataset_length = 30000
 partition_size = 10000
 
 num_partitions = dataset_length // partition_size
 print(f"Number of partitions: {num_partitions}")
+processed_partitions = []
 
-for i in range(dataset_length//partition_size):
+for i in range(num_partitions):
     print(f"Partition {i}")
     start = i * partition_size
     end = (i + 1) * partition_size
@@ -32,6 +37,10 @@ for i in range(dataset_length//partition_size):
     new_dataset = Dataset.from_dict({"input_ids": chunks})
     end_time = time.time()
     print(f"Time taken to chunk: {end_time - start_time}")
-    push_name = f"amuvarma/emilia-30k-TTS-iter-partition-{i}-of-{dataset_length//partition_size}"
-    new_dataset = new_dataset.shuffle(seed=42)
+    push_name = f"{dsn}-{i}-of-{dataset_length//partition_size}"
     new_dataset.push_to_hub(push_name)
+    processed_partitions.append(new_dataset)
+
+
+combined_dataset = concatenate_datasets(processed_partitions)
+combined_dataset.push_to_hub(f"amuvarma/{dsn}-full-grouped")
