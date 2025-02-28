1e48eb7c4ae94d7bb8a4b7d70a5ecd4f3705ee12
Amartya Varma <amartya.varma@gmail.com>
2025-02-28 10:20:21 -0800

    f

--- SNAC_FINAL/combine_rows_8192.py
+++ SNAC_FINAL/combine_rows_8192.py
@@ -1,12 +1,14 @@
 from datasets import Dataset, load_dataset
 from itertools import chain
 import os
+import time
 
 
 dsn = "amuvarma/emilia-30k-TTS"
 dataset = load_dataset(dsn, split='train')
 print(dataset)
 
+start_time = time.time()
 dataset = dataset.remove_columns([col for col in dataset.column_names if col not in ['input_ids']])
 
 
@@ -18,7 +20,7 @@ chunks = [all_tokens[i*chunk_size:(i+1)*chunk_size] for i in range(num_chunks)]
 
 
 new_dataset = Dataset.from_dict({"input_ids": chunks})
-
+end_time = time.time()
 push_name = "amuvarma/emilia-30k-TTS-iter"
 new_dataset = new_dataset.shuffle(seed=42)
 new_dataset.push_to_hub(push_name)
