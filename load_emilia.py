35561f1a1b6dfa7a34e5b9373e8d05e5c852ef93
Amartya Varma <amartya.varma@gmail.com>
2025-02-27 23:44:55 -0800

    f

--- load_emilia.py
+++ load_emilia.py
@@ -8,14 +8,14 @@ import os
 
 print(os.cpu_count())
 
-# snapshot_download(
-#     repo_id=repo_id,
-#     repo_type="dataset",   
-#     revision="main",        
-#     max_workers=64, 
-#     allow_patterns=["Emilia/EN/*.tar"],       
-# )
+snapshot_download(
+    repo_id=repo_id,
+    repo_type="dataset",   
+    revision="main",        
+    max_workers=os.cpu_count(), 
+    allow_patterns=["Emilia/EN/*.tar"],       
+)
 
-# path = "Emilia/EN/*.tar"
-# dataset = load_dataset("amphion/Emilia-Dataset", data_files={"en": path}, split="en")
-# print(dataset[0]["json"])
+path = "Emilia/EN/*.tar"
+dataset = load_dataset("amphion/Emilia-Dataset", data_files={"en": path}, split="en")
+print(dataset[0]["json"])
