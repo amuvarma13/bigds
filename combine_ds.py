7ebc55b7faac6943e27c5fc28c0c1f3cae0439d0
Amartya Varma <amartya.varma@gmail.com>
2025-03-01 14:32:36 -0800

    f

---
+++ combine_ds.py
@@ -0,0 +1,21 @@
+from datasets import load_dataset, concatenate_datasets
+from huggingface_hub import snapshot_download
+
+def load_dataset(dsn):
+    snapshot_download(
+        repo_id=dsn,
+        repo_type="dataset",   
+        revision="main",        
+        max_workers=64,     
+    )
+    return load_dataset(dsn, split='train')
+
+accs = ["amuvarma", "CanopyLabs", "CanopyLabsElias", "eliasfiz", "CanopyElias", "akv13"]
+
+dsns = [f"amuvarma/emilia-snac-merged-{acc}-TTS-grouped-8192" for acc in accs]
+
+datasets = [load_dataset(dsn) for dsn in dsns]
+
+combined_dataset = concatenate_datasets(datasets)
+
+combined_dataset.push_to_hub(f"amuvarma/emilia-snac-merged-all-TTS-grouped-8192")
