fc4e7d65c1bcfab7c7117cf6aa11afdf3b0c7c52
Amartya Varma <amartya.varma@gmail.com>
2025-03-01 12:18:53 -0800

    f

---
+++ combine_splits.py
@@ -0,0 +1,14 @@
+from huggingface_hub import snapshot_download
+from datasets import load_dataset
+
+repo_id = "amuvarma/emilia-snac"
+
+snapshot_download(
+    repo_id=repo_id,
+    repo_type="dataset",   
+    revision="main",        
+    max_workers=64,     
+)
+
+load_dataset(repo_id, split="train")
+ 
