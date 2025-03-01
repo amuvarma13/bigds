c81ece1f14ae10367c576d9afae21dd2521f71cd
Amartya Varma <amartya.varma@gmail.com>
2025-02-28 18:43:10 -0800

    f

--- gemma/convert_iids_to_gemma.py
+++ gemma/convert_iids_to_gemma.py
@@ -6,9 +6,20 @@ def _load_dataset(dataset_name):
         repo_id=dataset_name,
         repo_type="dataset",   
         revision="main",        
-        max_workers=64         
+        max_workers=80         
     )
     return load_dataset(dataset_name, split="train")
 
 dataset = _load_dataset(dsn)
 print(dataset)
+
+amount_to_add = 127744
+def add_offset(batch):
+    # batch["codes"] is a list of lists; add 127744 to each integer
+    batch["codes"] = [[code + amount_to_add for code in codes_list] for codes_list in batch["codes"]]
+    return batch
+
+# Apply the function with batched=True for efficiency
+dataset = dataset.map(add_offset, batched=True)
+
+dataset.push_to_hub(f"{dsn}-gemma")
