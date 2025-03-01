3aae240ea8b4f28b3308f4b1e57ea604f46bde00
Amartya Varma <amartya.varma@gmail.com>
2025-03-01 12:43:33 -0800

    f

--- SNAC_FINAL/tts.py
+++ SNAC_FINAL/tts.py
@@ -68,7 +68,7 @@ def create_input_ids(example):
 
     return example
 
-ds = ds.map(create_input_ids, remove_columns=["text", "codes_list"])
+ds = ds.map(create_input_ids, num_proc=20, remove_columns=["text", "codes_list"])
 
 columns_to_keep = ["input_ids"]
 columns_to_remove = [col for col in ds.column_names if col not in columns_to_keep]
