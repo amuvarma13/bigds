3c70bf708f71fb72282637b0ec214cf2dc2d1c39
Amartya Varma <amartya.varma@gmail.com>
2025-02-28 18:32:34 -0800

    f

--- gemma/convert_iids_to_gemma.py
+++ gemma/convert_iids_to_gemma.py
@@ -1,5 +1,14 @@
 from datasets import load_dataset
+from huggingface_hub import snapshot_download
+dsn = "amuvarma/snac-raw-10m"
+def _load_dataset(dataset_name):
+    snapshot_download(
+        repo_id=dataset_name,
+        repo_type="dataset",   
+        revision="main",        
+        max_workers=64         
+    )
+    return load_dataset(dataset_name, split="train")
 
-dataset = load_dataset("amuvarma/snac-raw-10m", split='train')
-
+dataset = _load_dataset(dsn)
 print(dataset)
