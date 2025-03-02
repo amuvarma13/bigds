bf57d770c94248f93635bb2160e6b05cd2cf7007
Amartya Varma <amartya.varma@gmail.com>
2025-03-01 16:40:41 -0800

    f

---
+++ gemma/ds_subset.py
@@ -0,0 +1,18 @@
+from datasets import load_dataset
+import os
+from transformers import AutoTokenizer
+from huggingface_hub import snapshot_download
+
+user_name = "akv13"
+dsn = f"amuvarma/emilia-snac-merged-{user_name}-gemma-TTS"
+
+snapshot_download(
+    repo_id=dsn,
+    repo_type="dataset",   
+    revision="main",        
+    max_workers=64,     
+)
+
+ds = load_dataset(dsn, split='train')
+
+print(ds)
