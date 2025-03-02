f3178dabd9609851e1dfe8ecfb0f935c1f6d5f73
Amartya Varma <amartya.varma@gmail.com>
2025-03-01 16:15:55 -0800

    f

--- gemma/convert_iids_to_gemma.py
+++ gemma/convert_iids_to_gemma.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 from huggingface_hub import snapshot_download
 
-user_name = "CanopyLabs"
+user_name = "amuvarma"
 dsn = f"amuvarma/emilia-snac-merged-{user_name}"
 def _load_dataset(dataset_name):
     snapshot_download(
