6e82f755ad8cf76e4400b371808acfd2bd07e1d9
Amartya Varma <amartya.varma@gmail.com>
2025-03-02 10:55:44 -0800

    f

--- gemma/get2656.py
+++ gemma/get2656.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 from transformers import AutoTokenizer
 from huggingface_hub import snapshot_download
 
-dsn = "amuvarma/wikipedia-unfiltered-en-tokenised"
+dsn = "amuvarma/wikipedia-unfiltered-en-tokenised-grouped-2048"
 
 snapshot_download(
     repo_id=dsn,
@@ -13,34 +13,35 @@ snapshot_download(
 
 ds = load_dataset(dsn, split='train')
 
-from datasets import Dataset
-from tqdm import tqdm
+print(ds)
+# from datasets import Dataset
+# from tqdm import tqdm
 
-chunk_size = 100_000  # Adjust as needed
-total_tokens = 0
-ds_length = len(ds)
+# chunk_size = 100_000  # Adjust as needed
+# total_tokens = 0
+# ds_length = len(ds)
 
-for start_idx in range(0, ds_length, chunk_size):
-    end_idx = min(start_idx + chunk_size, ds_length)
-    # Select a chunk
-    ds_chunk = ds.select(range(start_idx, end_idx))
+# for start_idx in range(0, ds_length, chunk_size):
+#     end_idx = min(start_idx + chunk_size, ds_length)
+#     # Select a chunk
+#     ds_chunk = ds.select(range(start_idx, end_idx))
 
-    # Map to compute token counts in this chunk
-    ds_chunk = ds_chunk.map(
-        lambda ex: {"token_count": len(ex["input_ids"])},
-        num_proc=60,  # Parallel processing
-        batched=False
-    )
+#     # Map to compute token counts in this chunk
+#     ds_chunk = ds_chunk.map(
+#         lambda ex: {"token_count": len(ex["input_ids"])},
+#         num_proc=60,  # Parallel processing
+#         batched=False
+#     )
 
-    # Sum the token_count column for this chunk
-    total_tokens += sum(ds_chunk["token_count"])
+#     # Sum the token_count column for this chunk
+#     total_tokens += sum(ds_chunk["token_count"])
 
-print("Total number of tokens:", total_tokens)
+# print("Total number of tokens:", total_tokens)
 
-total_rows_1024 = total_tokens//1024
-total_rows_2656 = total_tokens//2656
-total_rows_2048 = total_tokens//2048
+# total_rows_1024 = total_tokens//1024
+# total_rows_2656 = total_tokens//2656
+# total_rows_2048 = total_tokens//2048
 
-print("Total number of rows (2656):", total_rows_2656)
-print("Total number of rows (2048):", total_rows_2048)
-print("Total number of rows (1024):", total_rows_1024)
+# print("Total number of rows (2656):", total_rows_2656)
+# print("Total number of rows (2048):", total_rows_2048)
+# print("Total number of rows (1024):", total_rows_1024)
