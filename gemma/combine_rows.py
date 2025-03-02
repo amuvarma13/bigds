5bf4ab6a791d6a559ce544ca442df42a3caa07c9
Amartya Varma <amartya.varma@gmail.com>
2025-03-01 19:38:37 -0800

    f

--- gemma/combine_rows.py
+++ gemma/combine_rows.py
@@ -4,13 +4,13 @@ import os
 import time
 
 
-user_name = "CanopyLabs"
+user_name = "amuvarma"
 dsn = f"amuvarma/emilia-snac-merged-{user_name}-gemma-TTS"
 
 dataset = load_dataset(dsn, split='train')
 print(dataset)
-dataset_length = 3073199
-partition_size = 510000
+dataset_length = 3145501
+partition_size = 520000
 chunk_size = 2656
 
 
