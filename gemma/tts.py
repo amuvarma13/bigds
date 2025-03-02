fea25aafba810b601e2cd888b91bd577443fabe4
Amartya Varma <amartya.varma@gmail.com>
2025-03-01 16:28:17 -0800

    f

--- gemma/tts.py
+++ gemma/tts.py
@@ -58,7 +58,7 @@ def create_input_ids(example):
     # example["attention_mask"] = [1] * len(input_ids)
     return example
 
-ds = ds.map(create_input_ids, num_proc=num_proc, remove_columns=["text", "text_tokens", "codes"], batched=True)
+ds = ds.map(create_input_ids, num_proc=num_proc, remove_columns=["text", "codes_list"], batched=True)
 
 columns_to_keep = ["input_ids"]
 columns_to_remove = [col for col in ds.column_names if col not in columns_to_keep]
