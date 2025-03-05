262b53bb2b3c978ae93a857f6ff02edd33befcef
Amartya Varma <amartya.varma@gmail.com>
2025-03-04 17:58:25 -0800

    f

--- combine_splits.py
+++ combine_splits.py
@@ -1,21 +1,31 @@
 from huggingface_hub import snapshot_download
 from datasets import load_dataset, concatenate_datasets
-repo_name = "CanopyLabsElias"
-repo_id = f"{repo_name}/emilia-snac"
 
-snapshot_download(
-    repo_id=repo_id,
-    repo_type="dataset",   
-    revision="main",        
-    max_workers=64,     
-)
+all_repo_names = ["CanopyLabsElias", "CanopyElias", "CanopyLabs", "amuvarma", "eliasfiz", "akv13"]
+all_datasets = []
+def merge_split(repo_name):
+    repo_id = f"{repo_name}/emilia-snac-with-speaker"
 
-ds = load_dataset(repo_id)
+    snapshot_download(
+        repo_id=repo_id,
+        repo_type="dataset",   
+        revision="main",        
+        max_workers=64,     
+    )
 
-all_split_datasets = [ds[split_name] for split_name in ds.keys()]
+    ds = load_dataset(repo_id)
 
-# Merge them into a single Dataset
-merged_dataset = concatenate_datasets(all_split_datasets)
+    all_split_datasets = [ds[split_name] for split_name in ds.keys()]
+
+    merged_dataset = concatenate_datasets(all_split_datasets)
+
+    all_datasets.append(merged_dataset)
+
+for repo_name in all_repo_names:
+    merge_split(repo_name)
+
+# Merge all datasets into a single Dataset
+merged_dataset = concatenate_datasets(all_datasets)
 
 print(merged_dataset)
 
