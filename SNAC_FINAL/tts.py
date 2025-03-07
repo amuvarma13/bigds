dcc2b567a2e65e74e0f7fff869a70910ef33bcd5
Amartya Varma <amartya.varma@gmail.com>
2025-03-06 18:02:16 -0800

    f

--- SNAC_FINAL/tts.py
+++ SNAC_FINAL/tts.py
@@ -1,4 +1,4 @@
-dsn = "amuvarma/enhanced_combined_with_source"
+dsn = "amuvarma/luna-multi"
 
 from datasets import load_dataset
 import os
@@ -15,7 +15,7 @@ snapshot_download(
 ds = load_dataset(dsn, split='train')
 
 
-push_name = f"{dsn}-TTS-src"
+push_name = f"{dsn}-TTS-emo_tagged"
 
 tokeniser_length = 128256
 start_of_text = 128000
@@ -47,7 +47,7 @@ ds = ds.filter(lambda x: len(x["codes_list"]) > 0)
 
 
 def create_input_ids(example):
-    text_ids = tokenizer.encode(example["text"] +" "+ f"<{example['source']}>", add_special_tokens=True)
+    text_ids = tokenizer.encode(example["emo_text"], add_special_tokens=True)
     text_ids.append(end_of_text)
     example["text_tokens"] = text_ids
     input_ids = (
@@ -64,7 +64,7 @@ def create_input_ids(example):
 
     return example
 
-ds = ds.map(create_input_ids, num_proc=20, remove_columns=["text", "codes_list"])
+ds = ds.map(create_input_ids, num_proc=20, remove_columns=["emo_text", "codes_list"])
 
 columns_to_keep = ["input_ids"]
 columns_to_remove = [col for col in ds.column_names if col not in columns_to_keep]
