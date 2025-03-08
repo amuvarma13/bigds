7a1ac45410bdf699b5687c975bfb291d38edae6b
Amartya Varma <amartya.varma@gmail.com>
2025-03-08 13:27:38 -0800

    f

--- create_delay_codes.py
+++ create_delay_codes.py
@@ -9,15 +9,28 @@ snapshot_download(
 )
 
 ds = load_dataset(dsn, split='train')
-# ds = ds.select(range(1000))
+ds = ds.select(range(1000))
 def map_function(example):
     codes = example['codes_list']
-    filtered_codes = []
+    # Make a copy so we can modify positions without losing the original values.
+    new_codes = codes.copy()
     
-    # Start at index 7 to “shift back” every 7th element (i.e. skip the original 0th)
-    for i in range(7, len(codes), 7):
-        # Append the element that was at the start of the block (now shifted back by 7 positions)
-        filtered_codes.append(codes[i])
+    # For every index that's a multiple of 7, starting at 0:
+    for i in range(0, len(codes), 7):
+        # For index 0, there’s no valid “-7” target so we replace it with the code 7 positions later (if available)
+        if i == 0:
+            if i + 7 < len(codes):
+                new_codes[i] = codes[i + 7]
+            else:
+                # If there isn’t an element at i+7, mark it for removal.
+                new_codes[i] = None
+        else:
+            # For any other index i that is a multiple of 7, shift the code back 7 positions.
+            # That is, place the code originally at position i into position i - 7.
+            new_codes[i - 7] = codes[i]
+    
+    # Now remove any None entries (which might appear if a block didn’t have enough elements to shift in)
+    filtered_codes = [code for code in new_codes if code is not None]
     
     example['codes_list'] = filtered_codes
     return example
