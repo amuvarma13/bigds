e27a6f8ad3ee1d84f8eb67656e6541c68ecc31a3
Amartya Varma <amartya.varma@gmail.com>
2025-03-08 16:33:56 -0800

    f

--- create_loss_mask.py
+++ create_loss_mask.py
@@ -1,14 +1,24 @@
 from datasets import load_dataset
 dsn = "amuvarma/emilia-snac-merged-all-TTS-grouped-8192-sample-1000"
 ds = load_dataset(dsn, split='train')
-
 def mask_out_ids(example):
-    # Create a 'labels' column that contains input_ids values only if they're less than 132362
-    example['labels'] = [token_id if token_id < 132362 else -100 for token_id in example['input_ids']]
+
+    threshold = 128266 + 4096      # 132362
+    lower_bound = 128266 + 4 * 4096  # 144650
+    upper_bound = 128266 + 5 * 4096  # 148746
+    
+    labels = []
+    for token_id in example['input_ids']:
+        if token_id < threshold or (lower_bound <= token_id < upper_bound):
+            labels.append(token_id)
+        else:
+            labels.append(-100)
+    
+    example['labels'] = labels
     
     # Create an 'attention_mask' with 1s for all tokens
     example['attention_mask'] = [1] * len(example['input_ids'])
-
+    
     
     return example
 
