6094abafe58402e8ecafcba4828b255e64684ac9
Amartya Varma <amartya.varma@gmail.com>
2025-03-10 14:30:16 -0700

    initial website

--- SNAC_FINAL/tts-smol.py
+++ SNAC_FINAL/tts-smol.py
@@ -33,7 +33,7 @@ pad_token = tokeniser_length + 7
 
 audio_tokens_start = tokeniser_length + 10
 
-tokenizer_name = "HuggingFaceTB/SmolLM2-135M-Instruc"
+tokenizer_name = "HuggingFaceTB/SmolLM2-135M-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 
 
